eaa659c3dc55d9361d824771597d42216780b35f
Scott <scott.vinay.93@gmail.com>
2020-07-05 00:16:11 +0100

    Scrap file added

---
+++ scrap.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  4 13:26:48 2020
+
+@author: Scott
+"""
+
+import time
+
+for i in range(5):
+    time.sleep(3)
+    print('1')
+print('2')
